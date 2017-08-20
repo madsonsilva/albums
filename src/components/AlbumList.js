b4b290bfe3784eb6767064b0945d6cacd7969f2b
Madson Silva <madsonsilva@gmail.com>
2017-08-20 02:00:21 -0300

    rendering a list of components

---
+++ src/components/AlbumList.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+
+  state = { albums: [] };
+
+  componentWillMount() {
+    console.log('Init axios fetch');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+          .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <Text key={album.title}> {album.title} </Text>);
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
